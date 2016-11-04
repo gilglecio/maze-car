f7926b6c0bcbb003e7dcdc22df2b5d119fab4464
Gilglécio Santos de Oliveira <gilglecio_dev@hotmail.com>
2016-11-03 21:45:04 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -19,15 +19,65 @@ const int SERVO_PIN = 10;
 
 const float MIN_DISTANCE_CM = 2;
 
-int servoAngles[] = {0, 45, 90, 135, 180};
-
 Ultrasonic ultrasonic(ULTRASON_TRIGGER, ULTRASON_ECHO);
 Servo servo;
 
-void setup () {
+void setSpeedMotors(int val) {
+  analogWrite(MOTOR1_SPEED, val);
+  analogWrite(MOTOR2_SPEED, val);
+}
+
+void breakMotors(int time = 0) {
+  setSpeedMotors(0);
   
-  servo.attach(SERVO_PIN);
-  servo.write(90);
+  delay(time);
+}
+
+void forward(int time = 0) {
+  motor1_CW();
+  motor2_CW();
+  delay(time);
+}
+
+void backward(int time = 0) {
+  motor1_CCW();
+  motor2_CCW();
+  delay(time);
+}
+
+void toTheRight(int time = 0) {
+  motor1_CCW();
+  motor2_CW();
+  delay(time);
+}
+
+void toTheLeft(int time = 0) {
+  motor1_CW();
+  motor2_CCW();
+  delay(time);
+}
+
+void motor1_CW() {
+  digitalWrite(MOTOR1_IN1, HIGH);
+  digitalWrite(MOTOR1_IN2, LOW);
+}
+
+void motor1_CCW() {
+  digitalWrite(MOTOR1_IN1, LOW);
+  digitalWrite(MOTOR1_IN2, HIGH);
+}
+
+void motor2_CW() {
+  digitalWrite(MOTOR2_IN1, LOW);
+  digitalWrite(MOTOR2_IN2, HIGH);
+}
+
+void motor2_CCW() {
+  digitalWrite(MOTOR2_IN1, HIGH);
+  digitalWrite(MOTOR2_IN2, LOW);
+}
+
+void setup () {
   
   pinMode(MOTOR1_SPEED, OUTPUT);
   pinMode(MOTOR1_IN1, OUTPUT);
@@ -45,96 +95,58 @@ void setup () {
   
   pinMode(SERVO_GND, OUTPUT);
   pinMode(SERVO_VCC, OUTPUT);
-  pinMode(SERVO_PIN, INPUT);
+  pinMode(SERVO_PIN, OUTPUT);
   
   digitalWrite(SERVO_GND, LOW);
   digitalWrite(SERVO_VCC, HIGH);
   
-  breakMotors();
+  servo.attach(SERVO_PIN);
+  servo.write(90);
   
-  setSpeedMotors(100);
+  breakMotors(0);
+  
+  setSpeedMotors(150);
+  
+  Serial.begin(9600);
 }
 
-void loop () {
-  
-  float distance = getDistance();
-  
-  if (distance <= MIN_DISTANCE_CM) {
-    breakMotors();
-    toTheLeft(2000);
-  }
-  
-  forward();
-}
-
-float getDistanceByAngle() {
-
-  int array_length = sizeof(servoAngles) -1;
-  float distances[array_length];
-  
-  for (int i = 0; i = array_length; i++) {
-    servo.attach(servoAngles[i]);
-    distances[servoAngles[i]] = getDistance();
+void moveServo(int angle = 0, int time = 0) {
+  if(angle >= 0 || angle <= 180) {
+    servo.write(angle);
   }
 }
 
 float getDistance() {
-  return ultrasonic.convert(ultrasonic.timing(), Ultrasonic::CM)
+  float cmMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  return cmMsec; 
 }
 
-void setSpeedMotors(int val) {
-  analogWrite(MOTOR1_SPEED, val);
-  analogWrite(MOTOR2_SPEED, val);
-}
+// int servoAngles[] = {175, 127, 80, 47, 0};
 
-void breakMotors() {
-  digitalWrite(MOTOR1_IN1, HIGH);
-  digitalWrite(MOTOR1_IN2, HIGH);
-  
-  digitalWrite(MOTOR2_IN1, HIGH);
-  digitalWrite(MOTOR2_IN2, HIGH);
-}
+void loop () {
+ setSpeedMotors(115);
+ forward();
+ moveServo(80);
 
-void forward(int time = 0) {
-  motor1_CW();
-  motor2_CW();
-  delay(time);
-}
-
-void backward(int time = 0) {
-  motor1_CCW();
-  motor2_CCW();
-  delay(time);
-}
-
-void toTheLeft(int time = 0) {
-  motor1_CW();
-  motor2_CCW();
-  delay(time);
-}
-
-void toTheRight(int time = 0) {
-  motor1_CCW();
-  motor2_CW();
-  delay(time);
-}
-
-void motor1_CW() {
-  digitalWrite(MOTOR1_IN1, HIGH);
-  digitalWrite(MOTOR1_IN2, LOW);
-}
-
-void motor1_CCW() {
-  digitalWrite(MOTOR1_IN1, LOW);
-  digitalWrite(MOTOR1_IN2, HIGH);
-}
-
-void motor2_CW() {
-  digitalWrite(MOTOR2_IN1, HIGH);
-  digitalWrite(MOTOR2_IN2, LOW);
-}
-
-void motor2_CCW() {
-  digitalWrite(MOTOR2_IN1, LOW);
-  digitalWrite(MOTOR2_IN2, HIGH);
+  if(getDistance() < 20) {
+    breakMotors();
+    moveServo(175, 0);
+    float dLeft = getDistance();
+    delay(500);
+    moveServo(0, 0);
+    float dRight = getDistance();
+    delay(500);
+    
+    if(dLeft > dRight) {
+      Serial.println("Esquerda");
+        setSpeedMotors(115);
+       toTheLeft(400);
+    } else {
+        setSpeedMotors(115);
+       toTheRight(400);
+      Serial.println("Direita");
+    }
+  }
 }
